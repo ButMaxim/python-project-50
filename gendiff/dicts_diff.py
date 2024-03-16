e05372efe8e382ac4cc313c38b5a073d3a53d7fc
ButMaxim <46749095+ButMaxim@users.noreply.github.com>
2024-03-16 22:47:48 +0300

    Update dicts_diff.py

--- gendiff/dicts_diff.py
+++ gendiff/dicts_diff.py
@@ -1,40 +1,40 @@
-def build_diff(parced_data1: dict, parced_data2: dict):
+def build_diff(data1: dict, data2: dict):
     diff = list()
     sorted_keys = sorted(
-        list(set(parced_data1.keys()) | set(parced_data2.keys()))
+        list(set(data1.keys()) | set(data2.keys()))
     )
     for key in sorted_keys:
-        if key not in parced_data1:
+        if key not in data1:
             diff.append({
                 'key': key,
                 'operation': 'add',
-                'new': parced_data2[key]
+                'new': data2[key]
             })
-        elif key not in parced_data2:
+        elif key not in data2:
             diff.append({
                 'key': key,
                 'operation': 'removed',
-                'old': parced_data1[key]
+                'old': data1[key]
             })
-        elif isinstance(parced_data1[key], dict) and isinstance(
-                parced_data2[key], dict):
-            child = build_diff(parced_data1[key], parced_data2[key])
+        elif isinstance(data1[key], dict) and isinstance(
+                data2[key], dict):
+            child = build_diff(data1[key], data2[key])
             diff.append({
                 'key': key,
                 'operation': 'nested',
                 'value': child
             })
-        elif parced_data1[key] == parced_data2[key]:
+        elif data1[key] == data2[key]:
             diff.append({
                 'key': key,
                 'operation': 'same',
-                'value': parced_data1[key]
+                'value': data1[key]
             })
-        elif parced_data1[key] != parced_data2[key]:
+        elif data1[key] != data2[key]:
             diff.append({
                 'key': key,
                 'operation': 'changed',
-                'old': parced_data1[key],
-                'new': parced_data2[key]
+                'old': data1[key],
+                'new': data2[key]
             })
     return diff
